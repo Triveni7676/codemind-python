847cf84172b7768f3882613914c23d2d1a0b8908
Triveni7676 <98643153+Triveni7676@users.noreply.github.com>
2022-07-01 23:31:13 +0530

    n=int(input()) a=list(map(int,input().split())) p=[] k=0 for i in range(n):     c=0     while(a[i]>0):         r=a[i]%10         c+=1         a[i]=a[i]//10     #print(c)     p.append(c) x=max(p) y=p.count(x) print(y)

---
+++ Max_digits_count.py
@@ -0,0 +1,15 @@
+n=int(input())
+a=list(map(int,input().split()))
+p=[]
+k=0
+for i in range(n):
+    c=0
+    while(a[i]>0):
+        r=a[i]%10
+        c+=1
+        a[i]=a[i]//10
+    #print(c)
+    p.append(c)
+x=max(p)
+y=p.count(x)
+print(y)
